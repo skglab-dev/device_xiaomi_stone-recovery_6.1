f0a5d436af167bbb91999e90c5905ac7a6755df5
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2026-03-11 20:53:07 +0700

    stone: Initial port from spring to stone devices

--- twrp_stone.mk
+++ twrp_stone.mk
@@ -16,8 +16,8 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/xiaomi/spring/fox_spring.mk)
 
 ## Device identifier
-PRODUCT_DEVICE := spring
+PRODUCT_DEVICE := stone
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi 15 5G / 15R 5G / POCO M7+ 5G
+PRODUCT_MODEL := Redmi Note 12 5G / POCO X5 5G / Redmi Note 12R Pro 5G
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
