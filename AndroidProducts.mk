f3605ac808733d10ad8c206bccced8392b3de548
Kristoforus Ananta Putra Magallan <me@kristoforusapm.my.id>
2026-04-19 23:27:57 +0700

    Update product makefiles for twrp_stone

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@
 # Copyright (C) 2026 chickendrop89
 # SPDX-License-Identifier: GPL-3.0-only
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_spring.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_stone.mk
 
-COMMON_LUNCH_CHOICES := twrp_spring-ap2a-eng
+COMMON_LUNCH_CHOICES := twrp_stone-ap2a-eng
