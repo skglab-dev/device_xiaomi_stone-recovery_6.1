7945e2fa799f8afad1600407b08c2dc712a54e38
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2026-04-26 20:04:54 +0700

    boardconfig: Change the configuration from blair_sm6375-ac to holi and with other configuration to make it(maybe) boot in stone

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,13 @@ TARGET_CPU_VARIANT_RUNTIME := kryo300
 
 # Platform
 TARGET_BOOTLOADER_BOARD_NAME  := holi
-TARGET_BOARD_PLATFORM         := blair
+TARGET_BOARD_PLATFORM         := xiaomi_sm6375
 TARGET_BOARD_PLATFORM_GPU     := qcom-adreno619
 TARGET_USES_UEFI              := true
 BOARD_USES_QCOM_HARDWARE      := true
 
 # Kernel / Recovery image
-TARGET_PREBUILT_KERNEL        := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_KERNEL        := $(DEVICE_PATH)/prebuilt/Image
 TARGET_KERNEL_ARCH            := $(TARGET_ARCH)
 TARGET_KERNEL_HEADER_ARCH     := $(TARGET_ARCH)
 
@@ -114,3 +114,31 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USES_LOGD               := true
 # TARGET_RECOVERY_DEVICE_MODULES += strace
 # RECOVERY_BINARY_SOURCE_FILES   += $(TARGET_OUT_EXECUTABLES)/strace
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 200
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_APEX := true
+TW_HAS_EDL_MODE := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_FRAMERATE := 90
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone20/temp"
+TW_BATTERY_SYSFS_WAIT_SECONDS := 5
+TW_BACKUP_EXCLUSIONS := /data/fonts
+
+# TWRP Debug Flags
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
